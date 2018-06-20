5ef6de7f3b7f38c4b2b7502ebbfffcaba3043a3b
BriAnn Nicole Gibson <gibsonbriann@gmail.com>
2018-06-20 18:06:58 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,6 +1,6 @@
 your_favorite_word= "Love".upcase
 puts your_favorite_word
-your_favorite_word.upcase!
+your_favorite_word.upcase
 puts "IM NOT SHOUTING".downcase
 puts "supercalifragilisticexpialidocious".length
 a= "wow"
