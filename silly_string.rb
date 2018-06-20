442a658bee9141eadda52a54811252bf03580005
BriAnn Nicole Gibson <gibsonbriann@gmail.com>
2018-06-20 18:05:05 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -6,5 +6,5 @@ a= "wow"
 b= "mom"
 puts a+b
 puts "i really like programming".capitalize!
-String ="Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
-puts String.gsub "A","O"
+value ="Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
+puts value.gsub "A","O"
